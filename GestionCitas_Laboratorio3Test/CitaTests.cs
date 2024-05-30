e3f3f12f600732a4d85026736b6553160c343cde
OskyrGarcia <oscaradonaygarciazeledon@gmail.com>
2024-05-29 19:21:20 -0600

    Update CitaTests.cs

--- GestionCitas_Laboratorio3Test/CitaTests.cs
+++ GestionCitas_Laboratorio3Test/CitaTests.cs
@@ -8,15 +8,11 @@ public class CitaTests
     [Fact]
     public void CrearCita_PropiedadesAsignadasCorrectamente()
     {
-        
         var doctor = new Doctor("doctor1@example.com", "docpass1", "Dr. Smith");
         var paciente = new Paciente("patient1@example.com", "patientpass1", "Alice");
         var fecha = new DateTime(2024, 6, 1);
         var notas = "Revisión anual";
-
-        
         var cita = new Cita(paciente, doctor, fecha, notas);
-
         Assert.Equal(paciente, cita.Paciente);
         Assert.Equal(doctor, cita.Doctor);
         Assert.Equal(fecha, cita.Fecha);
